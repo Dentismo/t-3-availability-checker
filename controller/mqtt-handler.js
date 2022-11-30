aad0ea5ecce90f2ff3a9942bb8ee01eff62dcd95
johnchri <johnchri@student.chalmers.se>
2022-11-30 20:49:38 +0100

    #1 Add validation for incoming bookingRequests

--- controller/mqtt-handler.js
+++ controller/mqtt-handler.js
@@ -1,5 +1,7 @@
+const res = require('express/lib/response');
 const mqtt = require('mqtt');
 const checkAvailability = require('../controller');
+var Validator = require("jsonschema").Validator
 
 //const availabilityController = new AvailabilityController();
 
@@ -26,20 +28,85 @@ class MqttHandler {
     // Connection callback
     this.mqttClient.on('connect', () => {
       console.log(`mqtt client connected`);
-      client.subscribe('request/availablity', { qos: 1 });
+      client.subscribe('request/availability', { qos: 1 });
     });
 
 
     // When a message arrives, console.log it
     this.mqttClient.on('message', async function (topic, message) {
-      const result = await checkAvailability(JSON.parse(message.toString()));
-      if( result.accepted )
-        client.publish('request/createBooking', JSON.stringify(result))
-      else 
-        client.publish('response/createBooking', JSON.stringify(result))
-      
-      console.log(result)
+      var bookingRequest = JSON.parse(message)
+      if (validateBookingRequest(bookingRequest).length == 0) {
+        const result = await checkAvailability(JSON.parse(message.toString()));
+        if (result.accepted) {
+          client.publish('request/createBooking', JSON.stringify(result))
+        } else {
+          client.publish('response/createBooking', JSON.stringify(result))
+          console.log(result)
+        }
+      } else {
+        client.publish('response/createBooking', 'Invalid booking request body')
+      }
     });
+
+    function validateBookingRequest(bookingRequest) {
+      const errors = [];
+      const stateEnum = ['approved', 'denied', 'pending']
+
+      if (!("name" in bookingRequest.user)) {
+        errors.push('name: missing')
+      } else if (typeof bookingRequest.user.name !== 'string') {
+        errors.push('name: must be type string')
+      }
+
+      if (!("email" in bookingRequest.user)) {
+        errors.push('email: missing')
+      } else if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(bookingRequest.email)) {
+        errors.push('email: must be a valid email')
+      }
+
+      if (!("clinicId" in bookingRequest)) {
+        errors.push('clinicId: missing')
+      } else if (typeof bookingRequest.clinicId !== 'string') {
+        errors.push('clinicId: must be type string')
+      } else if(bookingRequest.clinicId.length < 1){
+        errors.push('clinicId: invalid length')
+      }
+
+      if (!("issuance" in bookingRequest)) {
+          errors.push('issuance: missing')
+        } else if (typeof bookingRequest.issuance !== 'string') {
+          errors.push('issuance: must be type string')
+        } else if(bookingRequest.issuance.length < 1 || bookingRequest.issuance.length > 13){
+          errors.push('issuance: invalid length')
+        }
+
+      if (!("date" in bookingRequest)) {
+        errors.push('date: missing')
+      } else if (typeof bookingRequest.date !== 'string') {
+        errors.push('date: must be type string')
+      }
+
+      if (!("state" in bookingRequest)) {
+        errors.push('state: missing')
+      } else if (typeof bookingRequest.state !== 'string') {
+        errors.push('state: must be type string')
+      } else if (!(stateEnum.includes(bookingRequest.state))) {
+        errors.push('state: must have a value of approved, pending or denied')
+      }
+
+      if (!("start" in bookingRequest)) {
+        errors.push('start: missing')
+      } else if (typeof bookingRequest.start !== 'string') {
+        errors.push('start: must be type string')
+      }
+
+      if (!("end" in bookingRequest)) {
+        errors.push('end: missing')
+      } else if (typeof bookingRequest.end !== 'string') {
+        errors.push('end: must be type string')
+      }
+      return errors
+    }
   }
 }
 
